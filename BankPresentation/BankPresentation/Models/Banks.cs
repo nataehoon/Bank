1c53c2f0dd8afccc1d25b220413d2dd4bab1fff3
nth <skeoddla@gmail.com>
2024-06-24 12:07:59 +0900

    setting

---
+++ BankPresentation/BankPresentation/Models/Banks.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankPresentation.Models
+{
+    internal class Banks
+    {
+        public string Id { get; set; }
+        public string BankName { get; set; }
+        public string BankCus { get; set; }
+    }
+}
---
+++ BankPresentation/BankPresentation/Models/Members.cs
@@ -0,0 +1,11 @@
+﻿namespace BankPresentation.Models
+{
+    internal class MemberInfo
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string PW { get; set; }
+        public string Email { get; set; }
+
+    }
+}
