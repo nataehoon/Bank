27ff8cfe1dc6f252341c5b60ec97da69a3d0efd2
nth <skeoddla@gmail.com>
2024-06-27 12:14:26 +0900

    addbank & admin

---
+++ BankPresentation/BankPresentation/Models/MyBankInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace BankPresentation.Models
+{
+    public class MyBankInfo
+    {
+        public string LINK_ID { get; set; }
+        public string USER_ID { get; set; }
+        public string BANK_ID { get; set; }
+        public string Asset { get; set; }
+    }
+}
